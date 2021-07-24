a8f9eda0984e6a3fb774685ea6721389dbb5f80c
Code Guru Mouse <phuoc2311@gmail.com>
2021-07-24 16:19:27 +0700

    updated passing code using lambdas syntax

--- src/cgmouse/javainaction/chap1/passcode/FilteringApplesTestDriveJava8.java
+++ src/cgmouse/javainaction/chap1/passcode/FilteringApplesTestDriveJava8.java
@@ -1,17 +1,28 @@
 package cgmouse.javainaction.chap1.passcode;
 import java.util.List;
 import cgmouse.javainaction.util.Apple;
+import cgmouse.javainaction.util.Corlor;
 import cgmouse.javainaction.util.Util;
 
 public class FilteringApplesTestDriveJava8 {
     public static void main(String[] args) {
-        List<Apple> lApples = FilteringApplseJava8
-        .filterApplesJava8(Util.inventoryApples, FilteringApplseJava8::isGreenApple);
+        List<Apple> lApples = FilteringApplesJava8
+        .filterApples(Util.inventoryApples, FilteringApplesJava8::isGreenApple);
         Util.printAppleList(lApples);
 
-        List<Apple> applesHeavyThan150 = FilteringApplseJava8
-        .filterApplesJava8(Util.inventoryApples, FilteringApplseJava8::isHeavyApple);
+        List<Apple> applesHeavyThan150 = FilteringApplesJava8
+        .filterApples(Util.inventoryApples, FilteringApplesJava8::isHeavyApple);
         Util.printAppleList(applesHeavyThan150);
+
+        //using lambdas syntax
+
+        List<Apple> greApples = FilteringApplesJava8
+        .filterApples(Util.inventoryApples, (Apple a) -> Corlor.RED.equals(a.getCorlor()));
+
+        Util.printAppleList(greApples);
+
+        //using library methods
+        
     }
     
 }
