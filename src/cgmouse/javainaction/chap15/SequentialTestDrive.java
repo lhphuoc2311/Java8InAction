5be17f8f91afd6d1bbc1dc34e6b77a5073fc6237
Code Guru Mouse <phuoc2311@gmail.com>
2021-08-12 21:25:56 +0700

    updated chapter 15's code

--- src/cgmouse/javainaction/chap15/SequentialTestDrive.java
+++ src/cgmouse/javainaction/chap15/SequentialTestDrive.java
@@ -4,6 +4,8 @@ public class SequentialTestDrive {
     public static void main(String[] args) {
         int x = 5;
         SequentialSimulate.sequential(x);
+        long l = 1_000_000;
+        System.out.println(l);
     }
     
 }
