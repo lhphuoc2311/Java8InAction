57b7694d8f9485275b55dcbbf2e6f9284f8b14b1
Code Guru Mouse <phuoc2311@gmail.com>
2021-08-15 10:17:18 +0700

    updated Anonymous class for sorting inventory

--- src/cgmouse/javainaction/chap3/SortingApplesTestDrive.java
+++ src/cgmouse/javainaction/chap3/SortingApplesTestDrive.java
@@ -1,5 +1,6 @@
 package cgmouse.javainaction.chap3;
 
+import java.util.Comparator;
 import java.util.List;
 
 import cgmouse.javainaction.util.Apple;
@@ -9,7 +10,14 @@ public class SortingApplesTestDrive {
     public static void main(String[] args) {
         List<Apple> inventory = Util.inventoryApples;
 
-        inventory.sort(new AppleComparator());
+        inventory.sort(new Comparator<Apple>(){
+
+            @Override
+            public int compare(Apple o1, Apple o2) {
+                return o1.getWeight().compareTo(o2.getWeight());
+            }
+            
+        });
 
         Util.printAppleList(inventory);
     }
