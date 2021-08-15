3342d80b810f50525557bccde08ac59119047e24
Code Guru Mouse <phuoc2311@gmail.com>
2021-08-15 11:04:54 +0700

    updated using comparing in more compact

--- src/cgmouse/javainaction/chap3/SortingApplesTestDrive.java
+++ src/cgmouse/javainaction/chap3/SortingApplesTestDrive.java
@@ -1,18 +1,16 @@
 package cgmouse.javainaction.chap3;
 
-import java.util.Comparator;
 import java.util.List;
 
 import cgmouse.javainaction.util.Apple;
 import cgmouse.javainaction.util.Util;
+import static java.util.Comparator.comparing;
 
 public class SortingApplesTestDrive {
     public static void main(String[] args) {
         List<Apple> inventory = Util.inventoryApples;
 
-        Comparator<Apple> c = Comparator.comparing((a) -> a.getWeight());
-
-        inventory.sort(c);
+        inventory.sort(comparing((a) -> a.getWeight()));
 
         Util.printAppleList(inventory);
     }
