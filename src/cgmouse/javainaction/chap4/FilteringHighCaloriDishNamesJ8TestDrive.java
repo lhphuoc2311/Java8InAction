39de255ca3cb47566465b2dbde98766f66f31350
Code Guru Mouse <phuoc2311@gmail.com>
2021-08-16 23:34:30 +0700

    updated using intermediate operations

--- src/cgmouse/javainaction/chap4/FilteringHighCaloriDishNamesJ8TestDrive.java
+++ src/cgmouse/javainaction/chap4/FilteringHighCaloriDishNamesJ8TestDrive.java
@@ -9,8 +9,14 @@ public class FilteringHighCaloriDishNamesJ8TestDrive {
     public static void main(String[] args) {
         List<String> threeHighCaloricDishNames = 
         menu.stream()
-        .filter(dish -> dish.getCalories() > 300)
-        .map(Dish::getName)
+        .filter(dish -> {
+            System.out.println("filtering:" + dish.getName());
+            return dish.getCalories() > 300;
+        })
+        .map(dish ->{
+            System.out.println("mapping: " + dish.getName());
+            return dish.getName();
+        })
         .limit(3)
         .collect(java.util.stream.Collectors.toList());
 
