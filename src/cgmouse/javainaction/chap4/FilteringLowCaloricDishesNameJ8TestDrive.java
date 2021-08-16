3b939d45dd2bb6d521aaebf3702f200835180bb4
Code Guru Mouse <phuoc2311@gmail.com>
2021-08-16 12:22:02 +0700

    added processing Collection with Java 8 Streams

---
+++ src/cgmouse/javainaction/chap4/FilteringLowCaloricDishesNameJ8TestDrive.java
@@ -0,0 +1,30 @@
+package cgmouse.javainaction.chap4;
+import static cgmouse.javainaction.chap4.Dish.menu;
+import static java.util.Comparator.comparing;
+import static java.util.stream.Collectors.toList;
+
+import java.util.List;
+import java.util.function.Function;
+
+public class FilteringLowCaloricDishesNameJ8TestDrive {
+    public static void main(String[] args) {
+        
+        Function<Dish, Integer> function = dish -> dish.getCalories();
+                                           //Dish::getCalories
+
+        List<String> lowCaloricDishesName =
+            menu.stream()
+            .filter(dish -> dish.getCalories() < 400)
+            .sorted(comparing(Dish::getCalories))
+            .map(Dish::getName)
+            .collect(toList());
+
+        for (String string : lowCaloricDishesName) {
+            System.out.println(string);
+        }
+
+
+        
+    }
+    
+}
