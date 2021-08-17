d4f49999e5c520d3399a66bc2f878d5881dae587
Code Guru Mouse <phuoc2311@gmail.com>
2021-08-17 20:09:17 +0700

    Truncating Stream by limit() method

---
+++ src/cgmouse/javainaction/chap5/TruncatingStreamTestDrive.java
@@ -0,0 +1,21 @@
+package cgmouse.javainaction.chap5;
+
+import java.util.List;
+
+public class TruncatingStreamTestDrive {
+
+    public static void main(String[] args) {
+
+        //selecting the first three dishes that have more than 300 calories
+
+        List<Dish> threeDishGT300Caloric =
+            Dish.specialMenu.stream()
+            .filter(dish -> dish.getCalories() > 300)
+            .limit(2)
+            .collect(java.util.stream.Collectors.toList());
+
+        System.out.println(threeDishGT300Caloric);
+        
+    }
+    
+}
