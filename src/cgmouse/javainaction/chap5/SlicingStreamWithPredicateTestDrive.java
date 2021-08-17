6e25ec765b0ac5847a1286fec99824f31914487a
Code Guru Mouse <phuoc2311@gmail.com>
2021-08-17 17:49:09 +0700

    Slicing Stream by takeWhile method

---
+++ src/cgmouse/javainaction/chap5/SlicingStreamWithPredicateTestDrive.java
@@ -0,0 +1,30 @@
+package cgmouse.javainaction.chap5;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class SlicingStreamWithPredicateTestDrive {
+
+    public static void main(String[] args) {
+        List<Dish> specialMenu = Arrays.asList(new Dish("seasonal fruit", true, 120, Dish.Type.OTHER),
+                new Dish("prawns", false, 300, Dish.Type.FISH), new Dish("rice", true, 350, Dish.Type.OTHER),
+                new Dish("chicken", false, 400, Dish.Type.MEAT), new Dish("french fries", true, 530, Dish.Type.OTHER));
+
+        // select the dishes that have fewer than 320 calories
+        List<Dish> filteredMenu = specialMenu.stream().filter(dish -> dish.getCalories() < 320)
+                .collect(java.util.stream.Collectors.toList());
+
+        System.out.println(filteredMenu);
+
+        //slicing menu
+
+        List<Dish> slicedMenu1 = 
+            specialMenu.stream()
+            .takeWhile(dish -> dish.getCalories() < 320)
+            .collect(java.util.stream.Collectors.toList());
+        System.out.println("slicing: " + slicedMenu1);
+
+        
+    }
+
+}
