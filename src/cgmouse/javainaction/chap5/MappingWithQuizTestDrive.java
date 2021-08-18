641336b1a979f9945e6018cc8c60004d5ca45168
Code Guru Mouse <phuoc2311@gmail.com>
2021-08-18 23:05:22 +0700

    update wrong solution to return pairs numbers using map method

--- src/cgmouse/javainaction/chap5/MappingWithQuizTestDrive.java
+++ src/cgmouse/javainaction/chap5/MappingWithQuizTestDrive.java
@@ -20,6 +20,17 @@ public class MappingWithQuizTestDrive {
         List<Integer> numbers1 = Arrays.asList(1, 2, 3);
         List<Integer> numbers2 = Arrays.asList(3, 4);
 
+        
+        //return  [(1, 3), (1, 4), (2, 3), (2, 4),(3, 3), (3, 4)]
+
+        //this is wrong
+        List<int[]> pairs = 
+            numbers1.stream()//Stream<Integer>
+                .map(num1 -> numbers2.stream()
+                                    .map(num2 -> new int[]{num1, num2})
+                    )//Stream<Stream<Integer[]>>
+                .collect(java.util.stream.Collectors.toList());
+
     }
     
 }
