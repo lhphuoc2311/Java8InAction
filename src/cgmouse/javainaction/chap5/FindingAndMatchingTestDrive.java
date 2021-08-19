0c261ce7eb9cdf0544961b007e94b7b7b0090a43
Code Guru Mouse <phuoc2311@gmail.com>
2021-08-19 11:12:15 +0700

    updated using terminal operation noneMatch

--- src/cgmouse/javainaction/chap5/FindingAndMatchingTestDrive.java
+++ src/cgmouse/javainaction/chap5/FindingAndMatchingTestDrive.java
@@ -8,13 +8,17 @@ public class FindingAndMatchingTestDrive {
             System.out.println("The menu is vegetarian friendly !!");
         }
 
-        
+
 
         //find out whether the menu is healthy (all dishes are below 1000 calories)
         boolean isHealthy = Dish.menu.stream()
                                     .allMatch(dish -> dish.getCalories() < 1000);
 
         System.out.println(isHealthy);
+
+        //using terminal operation noneMatch method
+        boolean isHealthy1 = Dish.menu.stream()
+                                    .noneMatch(dish -> dish.getCalories() >= 1000);
     }
     
 }
