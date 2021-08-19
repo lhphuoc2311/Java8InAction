de21d91c673d82466d938742c9de83fa6feb8087
Code Guru Mouse <phuoc2311@gmail.com>
2021-08-19 11:02:38 +0700

    updated using terminal operation all Match method

--- src/cgmouse/javainaction/chap5/FindingAndMatchingTestDrive.java
+++ src/cgmouse/javainaction/chap5/FindingAndMatchingTestDrive.java
@@ -7,6 +7,14 @@ public class FindingAndMatchingTestDrive {
         if(Dish.menu.stream().anyMatch(Dish::isVegetarian)){
             System.out.println("The menu is vegetarian friendly !!");
         }
+
+        
+
+        //find out whether the menu is healthy (all dishes are below 1000 calories)
+        boolean isHealthy = Dish.menu.stream()
+                                    .allMatch(dish -> dish.getCalories() < 1000);
+
+        System.out.println(isHealthy);
     }
     
 }
