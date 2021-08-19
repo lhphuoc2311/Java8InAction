92f70624927108e3ab926eb005aac0e9cc9237fc
Code Guru Mouse <phuoc2311@gmail.com>
2021-08-19 17:48:02 +0700

    added Reducing Stream with reduce method

---
+++ src/cgmouse/javainaction/chap5/ReducingStreamTestDrive.java
@@ -0,0 +1,25 @@
+package cgmouse.javainaction.chap5;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class ReducingStreamTestDrive {
+    public static void main(String[] args) {
+
+        List<Integer> someNumbers = Arrays.asList(1, 2, 3, 4, 5);
+
+        // Before using reduce method
+        int sum = 0;
+        for (Integer integer : someNumbers) {
+            sum += integer;
+        }
+
+        System.out.println(sum);
+
+        // Using reduce method over list of numbers to sum
+
+        int sum2 = someNumbers.stream().reduce(0, (num1, num2) -> num1 + num2);
+        System.out.println(sum2);
+    }
+
+}
