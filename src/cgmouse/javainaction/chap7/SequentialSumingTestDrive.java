c38679f70517eda245f9cc1bb0a3bf9aa1efa13f
Code Guru Mouse <phuoc2311@gmail.com>
2021-08-21 04:25:25 +0700

    added suming numbers sequentially by using Stream

---
+++ src/cgmouse/javainaction/chap7/SequentialSumingTestDrive.java
@@ -0,0 +1,17 @@
+package cgmouse.javainaction.chap7;
+
+import java.util.stream.Stream;
+
+public class SequentialSumingTestDrive {
+    public static void main(String[] args) {
+        long sum = sequentialSum(5);
+        System.out.println(sum);
+    }
+
+    public static long sequentialSum(long n) {
+            return Stream.iterate(1L, num -> num + 1)
+            .limit(n)
+            .reduce(0L, Long::sum);
+    }
+    
+}
