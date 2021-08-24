c59095827447cf8ac34a048a06d55dde4635d1ee
Code Guru Mouse <phuoc2311@gmail.com>
2021-08-24 20:30:37 +0700

    added initial MethodReferencesTestDrive

---
+++ src/cgmouse/javainaction/chap3/MethodReferencesTestDrive.java
@@ -0,0 +1,15 @@
+package cgmouse.javainaction.chap3;
+
+import java.util.List;
+
+import cgmouse.javainaction.util.Apple;
+import cgmouse.javainaction.util.Util;
+
+public class MethodReferencesTestDrive {
+    public static void main(String[] args) {
+
+     
+        
+    }
+    
+}
