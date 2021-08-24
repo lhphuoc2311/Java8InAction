b6638138b655b90e84d5d271d756218c73d09fe0
Code Guru Mouse <phuoc2311@gmail.com>
2021-08-24 20:31:32 +0700

    updated sorting by using lambda expression

--- src/cgmouse/javainaction/chap3/MethodReferencesTestDrive.java
+++ src/cgmouse/javainaction/chap3/MethodReferencesTestDrive.java
@@ -8,8 +8,11 @@ import cgmouse.javainaction.util.Util;
 public class MethodReferencesTestDrive {
     public static void main(String[] args) {
 
-     
-        
+        List<Apple> inventory = Util.inventoryApples;
+        // Using lambda expression
+        inventory.sort((a1, a2) -> a1.getWeight().compareTo(a2.getWeight()));
+
+        inventory.sort(java.util.Comparator.comparing(apple -> apple.getWeight()));
     }
-    
+
 }
